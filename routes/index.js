86186e035ad64e4c64bb66bf56de5d0244689e82
arpeggi-rpg <148833753+arpeggi-rpg@users.noreply.github.com>
2024-10-15 19:26:42 +0100

    Update bookings listing to list booking info

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TrainingBookings', bookings: ['a', 'b', 'c']});
+  res.render('index', { title: 'TrainingBookings', bookings: [{
+    name: "John Doe",
+    id: "1234567890",
+    bookingDate: new Date(2024, 12, 15),
+    creationDate: new Date()
+  }]});
 });
 
 module.exports = router;
