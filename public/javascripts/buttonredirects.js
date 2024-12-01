5a8ac7627bb3568cef2dfce5fb91181fc876078b
arpeggi-rpg <148833753+arpeggi-rpg@users.noreply.github.com>
2024-12-01 00:30:14 +0000

    Add required attribute to forms

--- public/javascripts/buttonredirects.js
+++ public/javascripts/buttonredirects.js
@@ -4,7 +4,7 @@ for (let button of editButtons) {
     button.addEventListener("click", () => {
         let parentRow = button.parentNode.parentNode;               // get row button is inside of
         let id = parentRow.children[1].textContent;                 // get id from row
-        let url = window.location.href + "update?id=" + id;           // create url to edit booking with that id
+        let url = window.location.href + "update?id=" + id;         // create url to edit booking with that id
         window.location.href = url;                                 // perform the redirect
     });
 }
@@ -16,9 +16,9 @@ for (let button of deleteButtons) {
         let parentRow = button.parentNode.parentNode;               // get row button is inside of
         let id = parentRow.children[1].textContent;                 // get id from row
         let url = window.location.href + "delete?id=" + id;         // create url to delete booking with that id
-        fetch(url, {
+        fetch(url, {                                                // perform the deletion
             method: "DELETE"
         })
-        .then(() => {window.location.reload()});
+        .then(() => {window.location.reload()});                    // reload the page to show results
     });
 }
